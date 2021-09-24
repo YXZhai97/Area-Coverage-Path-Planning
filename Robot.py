8bb0c84f2542d167d3caaa153349a472d56bf1dd
Eason <eason.zhai97@outlook.com>
2021-09-24 18:05:55 +0200

    define robot class

---
+++ Robot.py
@@ -0,0 +1,55 @@
+
+'''
+Define alpha agent
+The property of the alpha agent is defined
+The method of the alpha agent is defined
+
+'''
+
+class AlphaAgent:
+    number_of_agent=0 # the number of agent defined
+
+    def __init__(self):
+        self.id=self.number_of_agent
+        self.rs=10
+        self.rc=60
+        self.map=[]
+        self.initial_state=[]
+        self.state=[]
+        self.neighbour=[]
+        AlphaAgent.add_agent() # call the classmethod when initialize the object
+
+    @classmethod
+    def add_agent(cls):
+        cls.number_of_agent += 1
+
+
+
+class Envionment:
+    def __init__(self):
+        pass
+
+    def add_obstacle(self):
+
+
+class Obstacle:
+
+    def __init__(self):
+        pass
+
+
+
+
+
+# Test
+agent1=AlphaAgent()
+print(agent1.id)
+print(AlphaAgent.number_of_agent)
+print(agent1.number_of_agent)
+
+agent2=AlphaAgent()
+print(agent2.id)
+print(AlphaAgent.number_of_agent)
+print(agent2.number_of_agent)
+
+
