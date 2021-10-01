bc8c06e26c6919e6e7284f8e396ac840127dabd5
Eason <eason.zhai97@outlook.com>
2021-10-01 12:10:20 +0200

    define add_polygon method

--- sample/2dMap.py
+++ sample/2dMap.py
@@ -1,4 +1,3 @@
-
 '''
 define 2D map
 enable adding different shape of obstacles
@@ -9,25 +8,21 @@ import matplotlib.pyplot as plt
 from typing import List
 import math
 
+
 class EnvMap:
     def __init__(self, x_bound, y_bound, grid_length):
-        self.x_bound=x_bound
-        self.y_bound=y_bound
-        self.grid_length=grid_length
-        self.x_n=int(self.x_bound/self.grid_length)
-        self.y_n=int(self.y_bound/self.grid_length)
-        self.grid_map=self.make_grid(self.x_n,self.y_n)
+        self.x_bound = x_bound
+        self.y_bound = y_bound
+        self.grid_length = grid_length
+        self.x_n = int(self.x_bound / self.grid_length)
+        self.y_n = int(self.y_bound / self.grid_length)
+        self.grid_map = self.make_grid(self.x_n, self.y_n)
 
+    def make_grid(self, row, col):
 
+        return np.zeros((row, col))
 
-    def make_grid(self,row, col):
-
-        return np.zeros((row,col))
-
-
-
-
-    def add_circle(self,x_position,y_position,r):
+    def add_circle(self, x_position, y_position, r):
         '''
         Args:
             x_position: center point of the circle in x coordinate
@@ -38,36 +33,43 @@ class EnvMap:
         '''
 
         # get the grid_length from class attribute
-        grid_length=self.grid_length
+        grid_length = self.grid_length
 
-        # calculate center point of each grid
-        center_point=[]
+        # tolerance of the circle radius
+        r_t = r / 20
 
-        # set grid point inside the circle to 1
+        # set grid point near the circle boundary to 1
         for i in range(self.x_n):
             for j in range(self.y_n):
-                center_point=[grid_length/2+i*grid_length,grid_length+j*grid_length]
-                distance=math.sqrt((center_point[0]-x_position)**2+(center_point[1]-y_position)**2)
-                if distance<=r :
-                    self.grid_map[i,j]=1
+                center_point = [grid_length / 2 + i * grid_length, grid_length / 2 + j * grid_length]
+                distance = math.sqrt((center_point[0] - x_position) ** 2 + (center_point[1] - y_position) ** 2)
+                if abs(distance - r) < r_t:
+                    self.grid_map[i, j] = 1
 
-
-
-    def add_polygon(self,key_point:List[int]):
+    def add_polygon(self, key_points: List[int]):
         '''
 
         Args:
-            key_point: the vertex of the polygon
+            key_points: the vertex of the polygon [x1, y1, x2, y2, x3, y3]
 
         Returns: no return, the self.grid_map is modified
 
         '''
+        # append the first key_point to the end of the list
+        key_points.append(key_points[0])
+        key_points.append((key_points[1]))
 
+        # calculate the number of points
+        number_of_points = int(len(key_points) / 2 - 1)
 
-        pass
+        # iterate through the key_points and call add_line function
+        for i in range(number_of_points):
+            start_point = [key_points[2 * i], key_points[2 * i + 1]]
+            end_point = [key_points[2 * i + 2], key_points[2 * i + 3]]
+            self.add_line(start_point, end_point)
 
-    def add_irregular(self,start_point,matrix):
-        '''
+    def add_irregular(self, start_point, matrix):
+        """
 
         Args:
             start_point:
@@ -75,12 +77,11 @@ class EnvMap:
 
         Returns:
 
-        '''
-        pass
+        """
 
 
 
-    def add_line(self,p1:List, p2:List):
+    def add_line(self, p1: List, p2: List):
         '''
 
         Args:
@@ -139,25 +140,22 @@ class EnvMap:
 
     def floodFill(self, sr, sc, newColor):
 
-        R = len(self.grid_map)
-        C = len(self.grid_map[0])
+        row = len(self.grid_map)
+        col = len(self.grid_map[0])
         color = self.grid_map[sr][sc]
-
 
         def dfs(r, c):
             if self.grid_map[r][c] == color:
                 self.grid_map[r][c] = newColor
-                if r >= 1: dfs(r - 1, c)
-                if r + 1 < R: dfs(r + 1, c)
-                if c >= 1: dfs(r, c - 1)
-                if c + 1 < C: dfs(r, c + 1)
+                if r >= 1:
+                    dfs(r - 1, c)
+                if r + 1 < row:
+                    dfs(r + 1, c)
+                if c >= 1:
+                    dfs(r, c - 1)
+                if c + 1 < col: dfs(r, c + 1)
 
         dfs(sr, sc)
-
-
-
-
-
 
     def show_map(self):
         '''
@@ -168,34 +166,22 @@ class EnvMap:
         '''
 
         # define the color
-        color_map={1:np.array([0,0,0]), # 1 is obstacle filled with black color
-                   0:np.array([102,178,255])} # 0 is free space filled with blue color
+        color_map = {1: np.array([0, 0, 0]),  # 1 is obstacle filled with black color
+                     0: np.array([102, 178, 255])}  # 0 is free space filled with blue color
 
         # define a sD matrix to store the color value
-        data_3d=np.ndarray(shape=(self.x_n,self.y_n,3),dtype=int)
+        data_3d = np.ndarray(shape=(self.x_n, self.y_n, 3), dtype=int)
 
         # plot the grid_map with color
         for i in range(self.x_n):
             for j in range(self.y_n):
-                data_3d[i][j]=color_map[self.grid_map[i][j]]
+                data_3d[i][j] = color_map[self.grid_map[i][j]]
         plt.imshow(data_3d)
         plt.show()
 
 
-
-
-
-
-
-map=EnvMap(300,300,2)
-map.add_circle(90,60,40)
-map.add_line([100,100],[160,180])
-map.add_line([160,180],[100,250])
-map.add_line([100,250],[100,100])
-
-map.show_map()
-
-
-
-
-
+mymap = EnvMap(300, 300, 1)
+mymap.add_circle(90, 60, 30)
+mymap.add_circle(200,150,30)
+mymap.add_polygon([100,100,160,180,160,250,100,280])
+mymap.show_map()
