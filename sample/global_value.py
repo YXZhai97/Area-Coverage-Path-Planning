f01214e106dd64e822b44dd291c14b3f2cbc9fa6
“github’s <eason.zhai97@outlook.com>
2021-10-18 21:19:53 +0200

    add more global value

--- sample/global_value.py
+++ sample/global_value.py
@@ -14,3 +14,23 @@ c1_alpha=60
 c2_alpha=2*math.sqrt(c1_alpha)
 c1_beta=60
 c2_beta=2*math.sqrt(c1_beta)
+
+# grid map property
+x_bound=300
+y_bound=300
+grid_length=3
+x_n=int(x_bound/grid_length)
+y_n=int(y_bound/grid_length)
+
+# value in benefit function
+k1=0.04
+k2=0.01
+rohgamma=0.2
+
+# simulation
+T=30 # total simulation time
+step_size=0.01 #
+Iteration=int(T/step_size) # total iteration number
+
+
+
