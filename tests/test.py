5ae7b7698a8cf44d2cff0a3e08a34fc72866cfc8
“github’s <eason.zhai97@outlook.com>
2021-10-18 21:19:16 +0200

    add test case

--- tests/test.py
+++ tests/test.py
@@ -1,31 +1,37 @@
-import sample
+# import sample
+#
+#
+# agent1 = sample.AlphaAgent()
+# print(agent1.id)
+# print(sample.AlphaAgent.number_of_agent)
+# print(agent1.number_of_agent)
+#
+# agent2 = sample.AlphaAgent()
+# print(agent2.id)
+# print(sample.AlphaAgent.number_of_agent)
+# print(agent2.number_of_agent)
+#
+# # test floodFill
+# image = [
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+#     [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+#     [0, 0, 0, 1, 0, 0, 1, 0, 1, 1],
+#     [0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
+#     [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+# ]
+#
+# print(sample.floodFill(image, 3, 3, 1))
 
+import numpy as np
 
-agent1 = sample.AlphaAgent()
-print(agent1.id)
-print(sample.AlphaAgent.number_of_agent)
-print(agent1.number_of_agent)
-
-agent2 = sample.AlphaAgent()
-print(agent2.id)
-print(sample.AlphaAgent.number_of_agent)
-print(agent2.number_of_agent)
-
-# test floodFill
-image = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 0, 0, 1, 1],
-    [0, 0, 0, 1, 0, 0, 1, 0, 1, 1],
-    [0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-]
-
-print(sample.floodFill(image, 3, 3, 1))
+a=np.zeros((4,6,6))
+a[1,3,5]=2
+print(a[1])
