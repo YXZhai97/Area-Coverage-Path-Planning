f3494fcd27ac0e151ceeed2d94da07f375d9d032
“github’s <eason.zhai97@outlook.com>
2021-10-26 15:07:39 +0200

    start combine all methods to a path planner

--- sample/pathPlanning.py
+++ sample/pathPlanning.py
@@ -1,12 +1,38 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import robot
-import methods
+from robot import *
+import methods as m
 import env_map
 import global_value
 
 """
 The main script for path planning 
 """
+
+# robot initialization
+# define three robot and initialize the state and target
+define_robot(3)
+for robot in gv.robotList:
+    robot.random_init_state()
+    robot.random_init_target()
+
+show_robot(gv.robotList)
+
+
+
+
+
+# environment initialization
+
+
+# iteration
+
+
+# state and map update
+
+
+# plot the path
+# 2D animation
+# 2D static
 
