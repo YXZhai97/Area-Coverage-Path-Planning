eba888781857795e3fb1a2778ba05b4856b50f08
“github’s <eason.zhai97@outlook.com>
2021-11-12 11:27:20 +0100

    implement tangentbug

--- sample/bugalgorithm.py
+++ sample/bugalgorithm.py
@@ -14,12 +14,25 @@ Iteration = 40
 
 def tangent_bug(start_point, goal_point, env_map):
     rs = 5
-    step_length = 1
+    step_length = 0.5
     state = np.zeros((Iteration, 2))
     x_n = len(env_map[0])  # number of col
     y_n = len(env_map)  # number of row
     obs_map = np.zeros((y_n, x_n))
     mode = 0  # motion to goal , mode=1-> boundary follow
+
+    """
+    steps to calculate endpoints
+    1. update obs_map
+    2. get island 
+    3. check the intersection island
+        * distance to m line smaller than grid length
+        * between state and goal 
+    4. get the max and min angle of the island points, store them as endpoints 
+    5. calculate the heuristic distance of these two points 
+    
+    """
+
 
     def get_center(row, col):
         x_center = col * gv.grid_length + gv.grid_length / 2
@@ -36,6 +49,9 @@ def tangent_bug(start_point, goal_point, env_map):
                     obs_map[row, col] = -1
 
     def get_endpoints():
+        pass
+
+    def get_heuristic_goal():
         pass
 
     def go_straight():
